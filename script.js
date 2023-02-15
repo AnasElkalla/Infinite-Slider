1e794ec78e872f9a2f3b23ba1a8145786e8eeb51
Anas Elkalla <79075490+AnasElkalla@users.noreply.github.com>
2023-02-15 13:11:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -55,8 +55,8 @@ const changer= function (div, direction) {const nextDiv = next(div, direction);
   }} 
 const changeSrc = (div, direction) => {
 let i = div.querySelector("img").getAttribute("src").match(/\d+/)[0];
-  if (start && i==="2") {return} 
-  else if(start && (i==="1"||i==="3")) {start=false;changer(div, direction) }
+  if (start && activeDiv===2) {return} 
+  else if(start && (activeDiv===1||activeDiv===3)) {start=false;changer(div, direction) }
   else{
   changer(div, direction) ;} 
   
